152c0a8ac58c4901c299b18a6160726fc7bff81a
mingxuezhang <zhangmx1997@foxmail.com>
2017-08-25 20:05:55 +0800

    update lots of files...

--- app/src/main/java/com/example/hys/checkinapp/Const.java
+++ app/src/main/java/com/example/hys/checkinapp/Const.java
@@ -5,11 +5,11 @@ public class Const {
 	/**
 	 * 图片上传路径
 	 */
-	public static final String UPLOAD_URL="http://192.168.1.188:8080/uploadImage/UploadServlet";
+	public static final String UPLOAD_URL="http://192.168.191.1:8080/HttpClientDemo/UploadImage";
 	
 	/**
 	 * 图片下载路径
 	 */
-	public static final String DOWNLOAD_URL="http://192.168.1.188:8080/uploadImage/upload/";
+	public static final String DOWNLOAD_URL="http://192.168.191.1:8080/HttpClientDemo/upload/";
 
 }
