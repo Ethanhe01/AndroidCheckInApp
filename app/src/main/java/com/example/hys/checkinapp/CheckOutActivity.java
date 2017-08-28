5ed7f7e1fa28ddafe8e0ee9bf2aeb836cf5900e0
mingxuezhang <zhangmx1997@foxmail.com>
2017-08-29 00:47:27 +0800

    add checkout and inquiry functions

---
+++ app/src/main/java/com/example/hys/checkinapp/CheckOutActivity.java
@@ -0,0 +1,293 @@
+package com.example.hys.checkinapp;
+
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.geocoder.GeocodeResult;
+import com.amap.api.services.geocoder.GeocodeSearch;
+import com.amap.api.services.geocoder.RegeocodeQuery;
+import com.amap.api.services.geocoder.RegeocodeResult;
+import com.example.hys.util.AMapUtil;
+import com.example.hys.util.Utils;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class CheckOutActivity extends AppCompatActivity implements AMapLocationListener,GeocodeSearch.OnGeocodeSearchListener {
+    public final static String EXTRA_MESSAGE = "com.example.hys.checkinapp.MESSAGE";
+
+    private AMapLocationClient locationClient = null;
+    private AMapLocationClientOption locationOption = null;
+    private TextView textView;
+    private String[] strMsg;
+    private com.amap.api.maps.AMap aMap;
+    private MapView mapView;
+    private GeocodeSearch geocoderSearch;
+    private Marker geoMarker;
+    private static LatLonPoint latLonPoint;
+
+    private EditText mCoursenumView=null;
+    private String CourseNum="";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_in);
+        textView = (TextView) findViewById(R.id.text_map);
+        mapView = (MapView) findViewById(R.id.map);
+        mCoursenumView = (EditText) findViewById(R.id.courseId);
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
+        Location();
+
+        /************* 点击“提交”按钮，将info写入数据库 ************/
+        Button mEmailSignInButton = (Button) findViewById(R.id.confirm_button);
+        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CourseNum = mCoursenumView.getText().toString();
+                recordCheckoutInfo();
+            }
+        });
+    }
+
+    private void recordCheckoutInfo() {
+        String longitude = strMsg[1];//定位到的经度
+        String latitude = strMsg[2];//定位到的纬度
+        String StudentID = LoginActivity.email;//当前用户账号
+        String OutTime = "";
+
+        /************** 获取北京时间，非系统时间 ****************/
+        SimpleDateFormat dff = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        URL url = null;//取得资源对象
+        try {
+            url = new URL("http://www.baidu.com");
+            URLConnection uc = null;//生成连接对象
+            uc = url.openConnection();
+            uc.connect(); //发出连接
+            long ld = uc.getDate(); //取得网站日期时间
+            Date date = new Date(ld); //转换为标准时间对象
+            OutTime = dff.format(date);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /****************** 连接服务器和DB *******************/
+        try {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost("http://192.168.191.1:8080/HttpClientDemo/Checkout");
+
+            List<NameValuePair> params1 = new ArrayList<NameValuePair>();
+            params1.add(new BasicNameValuePair("Longitude", longitude));
+            params1.add(new BasicNameValuePair("Latitude", latitude));
+            params1.add(new BasicNameValuePair("CourseNum", CourseNum));
+            params1.add(new BasicNameValuePair("ID", StudentID));
+            params1.add(new BasicNameValuePair("OutTime", OutTime));
+            final UrlEncodedFormEntity entity = new UrlEncodedFormEntity(params1, "utf-8");
+            httpPost.setEntity(entity);
+            HttpResponse httpResponse = httpclient.execute(httpPost);
+            int i = httpResponse.getStatusLine().getStatusCode();
+            if (httpResponse.getStatusLine().getStatusCode() == 200) {
+                HttpEntity entity1 = httpResponse.getEntity();
+                String response = EntityUtils.toString(entity1, "utf-8");
+                if(response.equals("true"))
+                    Toast.makeText(CheckOutActivity.this, "签退信息提交成功", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(CheckOutActivity.this, "签退信息提交失败，请重试！", Toast.LENGTH_LONG).show();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void initMap(){
+
+        if (aMap == null) {
+            aMap = mapView.getMap();
+
+            //用高德默认图标
+            //geoMarker= aMap.addMarker(new MarkerOptions().anchor(0.5f, 0.5f).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+            //自定义图标
+            geoMarker = aMap.addMarker(new MarkerOptions().anchor(0.5f, 0.5f)
+                    .icon(BitmapDescriptorFactory.fromBitmap(BitmapFactory.decodeResource(getResources(), R.mipmap.punch_dw))));
+        }
+        geocoderSearch = new GeocodeSearch(this);
+        geocoderSearch.setOnGeocodeSearchListener(this);
+        getAddress(latLonPoint);
+    }
+
+
+    @Override
+    public void onLocationChanged(AMapLocation loc) {
+        if (null != loc) {
+            Message msg = mHandler.obtainMessage();
+            msg.obj = loc;
+            msg.what = Utils.MSG_LOCATION_FINISH;
+            mHandler.sendMessage(msg);
+        }
+
+    }
+
+    Handler mHandler = new Handler() {
+        public void dispatchMessage(Message msg) {
+            switch (msg.what) {
+                //定位完成
+                case Utils.MSG_LOCATION_FINISH:
+                    String result = "";
+                    String StudentID = LoginActivity.email;//获取当前用户账号
+                    try {
+                        AMapLocation loc = (AMapLocation) msg.obj;
+                        result = Utils.getLocationStr(loc, 1);
+                        strMsg = result.split(",");
+                        Toast.makeText(CheckOutActivity.this, "定位成功", Toast.LENGTH_LONG).show();
+
+
+
+                        textView.setText("地址：" + strMsg[0] + "\n" + "经    度：" + strMsg[1] + "\n" + "纬    度：" + strMsg[2] );
+                        latLonPoint= new LatLonPoint(Double.valueOf(strMsg[2]), Double.valueOf(strMsg[1]));
+                        initMap();
+                    } catch (Exception e) {
+                        Toast.makeText(CheckOutActivity.this, "定位失败", Toast.LENGTH_LONG).show();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        };
+
+    };
+
+    public void Location() {
+        // TODO Auto-generated method stub
+        try {
+            locationClient = new AMapLocationClient(this);
+            locationOption = new AMapLocationClientOption();
+            // 设置定位模式为低功耗模式
+            locationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Battery_Saving);
+            // 设置定位监听
+            locationClient.setLocationListener(this);
+            locationOption.setOnceLocation(true);//设置为单次定位
+            locationClient.setLocationOption(locationOption);// 设置定位参数
+            // 启动定位
+            locationClient.startLocation();
+            mHandler.sendEmptyMessage(Utils.MSG_LOCATION_START);
+        } catch (Exception e) {
+            Toast.makeText(CheckOutActivity.this, "定位失败", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    /**
+     * 响应逆地理编码
+     */
+    public void getAddress(final LatLonPoint latLonPoint) {
+        RegeocodeQuery query = new RegeocodeQuery(latLonPoint, 200,
+                GeocodeSearch.AMAP);// 第一个参数表示一个Latlng，第二参数表示范围多少米，第三个参数表示是火系坐标系还是GPS原生坐标系
+        geocoderSearch.getFromLocationAsyn(query);// 设置同步逆地理编码请求
+    }
+
+    /**
+     * 地理编码查询回调
+     */
+    @Override
+    public void onGeocodeSearched(GeocodeResult result, int rCode) {
+
+    }
+
+    /**
+     * 逆地理编码回调
+     */
+    @Override
+    public void onRegeocodeSearched(RegeocodeResult result, int rCode) {
+        if (rCode == 1000) {
+            if (result != null && result.getRegeocodeAddress() != null
+                    && result.getRegeocodeAddress().getFormatAddress() != null) {
+
+                Toast.makeText(CheckOutActivity.this,result.getRegeocodeAddress().getFormatAddress()
+                        + "附近",Toast.LENGTH_LONG).show();
+                aMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                        AMapUtil.convertToLatLng(latLonPoint), 15));
+                geoMarker.setPosition(AMapUtil.convertToLatLng(latLonPoint));
+            } else {
+
+            }
+        } else {
+        }
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+}
+
---
+++ app/src/main/java/com/example/hys/checkinapp/ServerUtils_Proof.java
@@ -0,0 +1,134 @@
+package com.example.hys.checkinapp;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class ServerUtils_Proof {
+
+	/**
+	 * 文件上传
+	 * 
+	 * @param urlStr  接口路径
+	 * @param filePath  本地图片路径
+	 * @return
+	 */
+
+	// Storage Permissions
+	/*private static final int REQUEST_EXTERNAL_STORAGE = 1;
+	private static String[] PERMISSIONS_STORAGE = {
+			Manifest.permission.READ_EXTERNAL_STORAGE,
+			Manifest.permission.WRITE_EXTERNAL_STORAGE
+	};*/
+
+	/**
+	 * Checks if the app has permission to write to device storage
+	 *
+	 * If the app does not has permission then the user will be prompted to grant permissions
+	 *
+	 * @param //activity
+	 */
+	/*public static void verifyStoragePermissions(Activity activity) {
+		// Check if we have write permission
+		int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+		if (permission != PackageManager.PERMISSION_GRANTED) {
+			// We don't have permission so prompt the user
+			ActivityCompat.requestPermissions(
+					activity,
+					PERMISSIONS_STORAGE,
+					REQUEST_EXTERNAL_STORAGE
+			);
+		}
+	}*/
+
+	public static String formUpload(String urlStr, String filePath) {
+		String rsp = "";
+		HttpURLConnection conn = null;
+		String BOUNDARY = "|"; // request头和上传文件内容分隔符
+		try {
+
+            /*List<NameValuePair> params1 = new ArrayList<NameValuePair>();
+            params1.add(new BasicNameValuePair("ID", LoginActivity.email));
+            final UrlEncodedFormEntity entity = new UrlEncodedFormEntity(params1, "utf-8");
+            conn.setEntity(entity);*/
+
+
+			URL url = new URL(urlStr);
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setConnectTimeout(5000);
+			conn.setReadTimeout(30000);
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			conn.setUseCaches(false);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Connection", "Keep-Alive");
+			conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.1; zh-CN; rv:1.9.2.6)");
+			conn.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + BOUNDARY);
+
+			OutputStream out = new DataOutputStream(conn.getOutputStream());
+			File file = new File(filePath);
+			String filename = file.getName();
+			String contentType = "";
+			if (filename.endsWith(".png")) {
+				contentType = "image/png";
+			}
+			if (filename.endsWith(".jpg")) {
+				contentType = "image/jpg";
+			}
+			if (filename.endsWith(".gif")) {
+				contentType = "image/gif";
+			}
+			if (filename.endsWith(".bmp")) {
+				contentType = "image/bmp";
+			}
+			if (contentType == null || contentType.equals("")) {
+				contentType = "application/octet-stream";
+			}
+			StringBuffer strBuf = new StringBuffer();
+			strBuf.append("\r\n").append("--").append(BOUNDARY).append("\r\n");
+			strBuf.append("Content-Disposition: form-data; name=\"" + filePath + "\"; filename=\"" + filename + "@" + LoginActivity.email + "@"
+					      +AppealActivity.CourseNum + "@" + AppealActivity.CurTime + "\"\r\n");
+			strBuf.append("Content-Type:" + contentType + "\r\n\r\n");
+			out.write(strBuf.toString().getBytes());
+			DataInputStream in = new DataInputStream(new FileInputStream(file));
+			int bytes = 0;
+			byte[] bufferOut = new byte[1024];
+			while ((bytes = in.read(bufferOut)) != -1) {
+				out.write(bufferOut, 0, bytes);
+			}
+			in.close();
+			byte[] endData = ("\r\n--" + BOUNDARY + "--\r\n").getBytes();
+			out.write(endData);
+			out.flush();
+			out.close();
+
+			// 读取返回数据
+			StringBuffer buffer = new StringBuffer();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				buffer.append(line).append("\n");
+			}
+			rsp = buffer.toString();
+			reader.close();
+			reader = null;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				conn.disconnect();
+				conn = null;
+			}
+		}
+		return rsp;
+	}
+
+}
